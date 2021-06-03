b70a3aefe7e74eb07ccfbcafce2199ee105f8997
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-06-03 08:06:04 +0530

    Update repo.py

--- userge/plugins/tools/repo.py
+++ userge/plugins/tools/repo.py
@@ -13,11 +13,11 @@ from userge import userge, Message, Config, versions, get_version
 async def see_repo(message: Message):
     """see repo"""
     output = f"""
-**Hey**, __I am using__ 🔥 **Userge** 🔥
+**Heya 🤗**, __I am using__ 🎁 **__Nivya__**🎁
 
     __Durable as a Serge__
 
-• **userge version** : `{get_version()}`
+• **nivya version** : `{get_version()}`
 • **license** : {versions.__license__}
 • **copyright** : {versions.__copyright__}
 • **repo** : [Userge]({Config.UPSTREAM_REPO})
